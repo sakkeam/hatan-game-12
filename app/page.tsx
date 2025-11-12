ff0abb6ac8af00652999362c9572e5b287286c64
Konosuke Sakai <konosukeam@gmail.com>
2025-11-12 20:14:11 +0900

    feat: memoize Canvas props for improved rendering performance

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@
 
 'use client';
 
+import { useMemo } from 'react';
 import { Canvas } from '@react-three/fiber';
 import GameScene from './components/GameScene';
 import GameUI from './components/GameUI';
@@ -13,15 +14,20 @@ import SwipeHandler from './components/SwipeHandler';
 import GameLoop from './game/loop';
 
 export default function Home() {
+  // Memoize Canvas props to prevent recreating objects on every render
+  const glProps = useMemo(() => ({ antialias: true, alpha: false }), []);
+  const dprRange = useMemo(() => [1, 2] as [number, number], []);
+  
   return (
     <div className="relative w-full h-screen overflow-hidden bg-black">
       {/* Three.js Canvas - Full screen portrait */}
       <Canvas
         className="absolute inset-0"
-        gl={{ antialias: true, alpha: false }}
-        dpr={[1, 2]}
+        gl={glProps}
+        dpr={dprRange}
       >
         <GameScene />
+      </Canvas>
       </Canvas>
 
       {/* UI Overlay */}
